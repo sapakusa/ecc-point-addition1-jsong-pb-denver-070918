823ff25004d07ce23dc82226aaedc07e52b00894
adamjonas <jonas@flatironschool.com>
2018-07-08 19:48:56 -0400

    first commit

---
+++ test/index_test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from ipynb.fs.full.index import *
+
+class PointTest(TestCase):
+
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
